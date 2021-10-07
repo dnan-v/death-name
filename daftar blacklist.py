05c69e5b7a97edab0517de18288229d40a639b3f
dnan-v <76686801+dnan-v@users.noreply.github.com>
2021-10-07 11:42:54 +0000

    subscribe xtacin

---
+++ daftar blacklist.py
@@ -0,0 +1,66 @@
+from string import printable
+import sys
+import timeit
+import logging
+import os
+import random
+
+logging.root
+logging.Template("selamat datang di tempat blacklist nama happy")
+
+os.write
+os.sys
+os.system("figlet death name")
+
+table = "selamat datang death lv semoga bersenang senang dengan tool ga jelas ini "
+print(table)
+
+
+
+now = timeit
+now = now.timeit()
+now = timeit.Timer
+now.timeit
+
+nama = input("masukan nama yang mau di blacklist : ")
+
+sys_config = sys.copyright
+sys_config = sys.exc_info
+sys_config = sys.exit
+
+os.system("figlet starting")
+
+class death_name(object):
+    def __init__(self,death):
+        self.death = death
+        
+        b = death_name("sssts")
+        print(death,b)
+        
+    a = "memulai melakukan starting name tersebut"
+        
+    if a:
+        print(a)
+     
+         
+    now(object)
+    load = "===========%1"
+    load = "===========%98"
+    load = "===========%100"
+    
+    loading = random.random
+    
+    while loading:
+        print(loading)
+
+        random.Random(loading)
+        loading = random.Random("loading")
+            
+        enable = "berhasil"
+        print("selamat anda berhasil semoga bahagia ", enable)
+        enable = True
+        
+        gagal = "gagal"
+        print("anda gagal", gagal)
+        gagal = False
+        
